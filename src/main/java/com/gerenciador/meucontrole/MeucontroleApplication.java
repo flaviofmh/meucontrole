f6a2ca391eaa9bb815f31ee60b2851cfb7bac731
silva-wellington <wsa.jpb@gmail.com>
2021-05-10 23:22:36 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/gerenciador/meucontrole/MeucontroleApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.meucontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucontroleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeucontroleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciador/meucontrole/MeucontroleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.meucontrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeucontroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
