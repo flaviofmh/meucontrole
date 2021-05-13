2b7e8fbb71eb4a36a354dba16fe29fbe3004b33d
silva-wellington <wsa.jpb@gmail.com>
2021-05-13 20:09:15 -0300

    Adicionado Model Mapper

--- src/main/java/com/gerenciador/meucontrole/MeuControleApplication.java
+++ src/main/java/com/gerenciador/meucontrole/MeuControleApplication.java
@@ -1,13 +1,20 @@
 package com.gerenciador.meucontrole;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MeucontroleApplication {
+public class MeuControleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MeucontroleApplication.class, args);
+		SpringApplication.run(MeuControleApplication.class, args);
 	}
+	
+	@Bean
+    public ModelMapper modelMapper() {
+	    return new ModelMapper();
+    }
 
 }
